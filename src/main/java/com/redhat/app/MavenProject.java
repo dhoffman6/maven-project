bef2ebbd5ce4af74dae549cc8373a004ee85d714
Doug <dhoffman@redhat.com>
2014-07-15 12:13:49 +1000

    First commit

---
+++ src/main/java/com/redhat/app/MavenProject.java
@@ -0,0 +1,9 @@
+
+package com.redhat.app;
+public class MavenProject {
+
+    public static void main(String[] args) {
+        System.out.println("I am a banana. Do you find me appeeling? ");
+    }
+
+}
