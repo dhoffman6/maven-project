ecb5228888f2fb056c248d6a8b793a7ca08e17c1
Doug <dhoffman@redhat.com>
2014-08-08 16:08:13 +1000

    Update to ouptut

--- src/main/java/com/redhat/app/MavenProject.java
+++ src/main/java/com/redhat/app/MavenProject.java
@@ -4,7 +4,7 @@ public class MavenProject {
 
     public static void main(String[] args) {
         System.out.println("I am a banana. Do you find me appeeling? ");
-	System.out.println("No. Not at all");
+	System.out.println("No. Not at all. And I do not appreciate such outright foolishness, sir.");
     }
 
 }
--- src/main/java/com/redhat/app/MavenProject.java~
+++ src/main/java/com/redhat/app/MavenProject.java~
@@ -4,6 +4,7 @@ public class MavenProject {
 
     public static void main(String[] args) {
         System.out.println("I am a banana. Do you find me appeeling? ");
+	System.out.println("No. Not at all");
     }
 
 }
